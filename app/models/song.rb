0af6293d1d0712463d3af758f88f353e4d7a0b64
Timothy Andrew <mail@timothyandrew.net>
2014-01-14 01:14:20 +0530

    Show the lists a song is in

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@
 
 class Song < ActiveRecord::Base
   has_many :song_list_items
-  has_many :songs, through: :song_list_items
+  has_many :song_lists, through: :song_list_items
 
   KEYS = %w(C C♯ D E♭ E F F♯ G A♭ A B♭ B)
 
