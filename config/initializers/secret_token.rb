52b4ea7b170d482c1ed5a5571432d202912c4f23
Timothy Andrew <mail@timothyandrew.net>
2014-01-13 10:59:27 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorshipTeamCenter::Application.config.secret_key_base = '4a76e7894a972c39d2afa20a9e9035411b046f71e5049214616f4041c5df82677723688eab160581404c2c38a89fcff4a059975efc7aa649bb13a7d577c531e1'
