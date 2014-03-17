3c02dea1876afcb1e37844ef0994eee4a1068fa8
Timothy Andrew <mail@timothyandrew.net>
2014-03-18 02:20:16 +0530

    Use a custom RegistrationsController
    Issue #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 WorshipTeamCenter::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :song_lists
   resources :songs
   root to: "song_lists#index"
