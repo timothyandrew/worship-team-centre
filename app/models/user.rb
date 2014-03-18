ed2609ac59ebefe6db072e1a6f10e5fb95c61c99
Timothy Andrew <mail@timothyandrew.net>
2014-03-18 12:24:38 +0530

    Add a user profile page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
+
+  has_many :team_memberships
+  has_many :song_lists, through: :team_memberships
+  has_many :leaderships, foreign_key: 'leader_id', class_name: SongList
+
+  def all_song_lists_a_part_of
+    (song_lists + leaderships).uniq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WorshipTeamCenter::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
+  resources :users, only: [:show]
   resources :song_lists
   resources :songs
   root to: "song_lists#index"
