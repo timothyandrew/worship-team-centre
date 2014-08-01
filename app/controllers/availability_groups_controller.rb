d2478c8782f3c279e38008c82aff11178e2c4a01
Timothy Andrew <mail@timothyandrew.net>
2014-08-01 14:58:17 +0530

    Track availability on mixpanel.

--- app/controllers/availability_groups_controller.rb
+++ app/controllers/availability_groups_controller.rb
@@ -7,6 +7,7 @@ class AvailabilityGroupsController < ApplicationController
     @availability_group = AvailabilityGroup.new(availability_group_params)
     @availability_group.user = current_user
     if @availability_group.save_with_availabilities(params[:availabilities])
+      track_event("Gave availability.", availability_group_params)
       redirect_to root_path, notice: "Your availability was saved!"
     else
       render :new
